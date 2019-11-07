673b0c9559cbeacca1ee19a4c7d8e1425f4f315c
Chris Ash <goodvibesjumper@hotmail.com>
2019-11-06 22:02:12 -0700

    .

--- src/main/java/com/nibblesonbits/yougotsurveyed/YougotsurveyedApplication.java
+++ src/main/java/com/nibblesonbits/yougotsurveyed/YougotsurveyedApplication.java
@@ -2,26 +2,16 @@ package com.nibblesonbits.yougotsurveyed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class YougotsurveyedApplication {
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/survey/**").allowedMethods("GET","POST","PUT", "DELETE");
-			}
-		};
-	}
 	public static void main(String[] args) {
-		SpringApplication.run(YougotsurveyedApplication.class, args);
+		final ApplicationContext ctx = SpringApplication.run(YougotsurveyedApplication.class, args);
+		final AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext();
+    	annotationConfigApplicationContext.register(CORSConfig.class);
+    	annotationConfigApplicationContext.refresh();
 	}
 
 }
