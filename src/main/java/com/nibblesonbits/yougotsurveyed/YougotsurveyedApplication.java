07f86d1f64834dff1129acce2c2f4e0f3e8a8754
Chris Ash <goodvibesjumper@hotmail.com>
2019-11-07 12:38:38 -0700

    cors working

--- src/main/java/com/nibblesonbits/yougotsurveyed/YougotsurveyedApplication.java
+++ src/main/java/com/nibblesonbits/yougotsurveyed/YougotsurveyedApplication.java
@@ -2,16 +2,23 @@ package com.nibblesonbits.yougotsurveyed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class YougotsurveyedApplication {
 	public static void main(String[] args) {
 		final ApplicationContext ctx = SpringApplication.run(YougotsurveyedApplication.class, args);
-		final AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext();
-    	annotationConfigApplicationContext.register(CORSConfig.class);
-    	annotationConfigApplicationContext.refresh();
+		//final AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext();
+    	//annotationConfigApplicationContext.register(CORSConfig.class);
+    	//annotationConfigApplicationContext.refresh();
 	}
 
 }
